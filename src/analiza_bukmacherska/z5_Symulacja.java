e7b5251b24c59dd472598dbb7e07648221737932
Pawel Pamula <pawel.pamula@gmail.com>
2013-01-28 21:34:09 +0100

    - Team Vs Team: - symulacja sytrategii

---
+++ src/analiza_bukmacherska/z5_Symulacja.java
@@ -0,0 +1,20 @@
+package analiza_bukmacherska;
+
+import javax.swing.JButton;
+import javax.swing.JLayeredPane;
+import javax.swing.JTextField;
+
+public class z5_Symulacja extends JLayeredPane{
+    
+    JTextField data1;
+    JTextField data2;
+    
+    JButton symuluj;
+    
+    
+    public z5_Symulacja()
+    {
+        //rozmiar okna
+        setBounds(0, 0, 1024, 530);
+    }
+}
