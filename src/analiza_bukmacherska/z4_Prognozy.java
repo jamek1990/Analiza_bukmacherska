ad2ed3b27f777f170ee0d40362591762dfc7989f
Leczyslaw <Leczyslaw@Leczyslaw-PC>
2013-01-30 22:53:22 +0100

    wersja 22.53, czcionki

--- src/analiza_bukmacherska/z4_Prognozy.java
+++ src/analiza_bukmacherska/z4_Prognozy.java
@@ -82,56 +82,64 @@ public class z4_Prognozy extends JLayeredPane{
         add(buttonAM);                            
         /////////////////////////////////////////////////////////Ramka na kwotę:        
         JLabel myMoneyText = new JLabel();
-        myMoneyText.setText("Posiadana kwota:");
-        myMoneyText.setBounds(10, 80, 115, 35);
+        myMoneyText.setText("POSIADANA KWOTA:");
+        myMoneyText.setBounds(10, 80, 125, 35);
         add(myMoneyText);
         
         myMoney = new JTextField();
         myMoney.setText("");
-        myMoney.setBounds(125, 75, 290, 35);
+        myMoney.setBounds(135, 75, 270, 35);
         add(myMoney);        
         /////////////////////////////////////////////////////////czesc dwa i pol          
         JLabel strAgr = new JLabel();
-        strAgr.setText("Metoda agresywna");
+        strAgr.setText("METODA AGRESYWNA");
         strAgr.setBounds(10, 380, 140, 35);
         
         JLabel strOpt = new JLabel();
-        strOpt.setText("Metoda optymalna");
+        strOpt.setText("METODA OPTYMALNA");
         strOpt.setBounds(150, 380, 140, 35);
        
         JLabel strLam = new JLabel();
-        strLam.setText("Metoda zachowawcza");
+        strLam.setText("METODA SPOKOJNA");
         strLam.setBounds(290, 380, 140, 35);
         
         add(strLam);
         add(strAgr);
         add(strOpt);
         ///////////////////////////////////////////////////////////czesc trzecia        
-        okienko = new m1_okienko(400,200,10,206,"Wybierz strategię gry");
+        okienko = new m1_okienko(400,200,10,206,"WYBIERZ STARTEGIĘ GRY");
         add(okienko);      
-        results = new m1_okienko(500,80,420,10,"Proponowana strategia gry w najbliższym czasie");
+        results = new m1_okienko(500,80,420,10,"PROPONOWANA TAKTYKA NA NAJBLIŻSZĄ KOLEJKĘ");
         add(results);
         textFrame = new JTextArea();   
-        jP_Prognozy = new m1_okienko(400,200,10,10,"Prognozy");
-        add(jP_Prognozy);        
-    }
-    private void setResult(Vector<mecz> strategy, int S){
-        if(strategy.isEmpty()){
-            opcja = new NastepnyWynik[0];
+        jP_Prognozy = new m1_okienko(400,200,10,10,"PROGNOZY");
+        add(jP_Prognozy); 
+        
+        opcja = new NastepnyWynik[0];
+            textFrame = new JTextArea();
             textFrame.setLineWrap(true);
             textFrame.setText("Niestety, ale obecnie \nnie ma ciekawych \nmeczów  w których \nopłacałoby \nsię ryzykować.");
             textFrame.setForeground(Color.RED);
-            textFrame.setBackground(getBackground());
+            textFrame.setBackground(new Color(238,238,238));
             textFrame.setEditable(false);
             textFrame.setFont(new Font("sansserif", Font.BOLD, 32));
             textFrame.setBounds(460, 120, 400, 300);
-            add(textFrame);
-            return;
+            textFrame.setVisible(false);
+            add(textFrame);  
+    }
+    private void setResult(Vector<mecz> strategy, int S){
+        if(strategy.isEmpty()){
+            int k = opcja.length;
+            while (k > 0){opcja[k].setVisible(false);--k;}
+            opcja = new NastepnyWynik[0];            
+            textFrame.setVisible(true);
         }
-        else{            
+        else{
             int N = strategy.size();
             System.out.println(N);
-            textFrame.setText("");
+            textFrame.setVisible(false);
+            int k = opcja.length;
+            while (k > 0){opcja[k].setVisible(false);--k;}
             opcja = new NastepnyWynik[N];
             double K = 1.0;
             try{
@@ -140,13 +148,13 @@ public class z4_Prognozy extends JLayeredPane{
             catch(Exception ex){}  
             for(int i = 0; i < N; i++){
                 System.out.println(strategy.get(i).wypisz());
-                opcja[i] = new NastepnyWynik(""+strategy.get(i).getStawka()*K,strategy.get(i).getTeam1(),strategy.get(i).getTeam2(),"" + (strategy.get(i).getData()%10000)/100 + "." +strategy.get(i).getData()%100,S);
+                opcja[i] = new NastepnyWynik(""+strategy.get(i).getStawka()*K,strategy.get(i).getTeam1().toUpperCase(),strategy.get(i).getTeam2().toUpperCase(),"" + (strategy.get(i).getData()%10000)/100 + "." +strategy.get(i).getData()%100,S);
                 opcja[i].setLocation(460, i*60+60);   
                 add(opcja[i]);
             }
         }
     }
-    public void lameMethode(){                 
+    public void lameMethode(){     
         setResult(test.getLameStrategy(),1);
     }  
     public void optimalMethode(){         
